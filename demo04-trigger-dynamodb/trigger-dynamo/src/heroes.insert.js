1b18b85ffa83ce47c9bf1446c513e3907a3cf473
Felipe Pires Soares <felipe.pires.soaresti@gmail.com>
2022-01-16 16:26:15 -0300

    finishing demo04

---
+++ demo04-trigger-dynamodb/trigger-dynamo/src/heroes.insert.js
@@ -0,0 +1,70 @@
+const uuid = require('uuid')
+const Joi = require('@hapi/joi')
+const decoratorValidator = require('./utils/decoratorValidator')
+const globalEnum = require('./utils/globalEnum')
+
+class Handler {
+    constructor({ dynamoDBSvc }) {
+        this.dynamoDBSvc = dynamoDBSvc
+        this.dynamodbTable = process.env.DYNAMODB_TABLE
+    }
+    static validator() {
+        return Joi.object({
+            nome: Joi.string().max(100).min(2).required(),
+            poder: Joi.string().max(20).required()
+        })
+    }
+    async insertItem(params) {
+        return this.dynamoDBSvc.put(params).promise()
+    }
+    prepareData(data) {
+        const params = {
+            TableName: this.dynamodbTable,
+            Item: {
+                ...data,
+                id: uuid.v1(),
+                createdAt: new Date().toISOString()
+            }
+        }
+        return params
+    }
+    handlerSucess(data) {
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(data)
+        }
+        return response
+    }
+    handleError(data) {
+        return {
+            statusCode: data.statusCode || 501,
+            headers: { 'Content-type': 'text/plain' },
+            body: 'Couldn\ t create item!!'
+        }
+    }
+    async main(event) {
+        try {
+            // agora o decorator modifica o body e já
+            // retorna no formato JSON
+            const data = event.body
+
+            const dbParams = this.prepareData(data)
+            await this.insertItem(dbParams)
+            return this.handlerSucess(dbParams.Item)
+        } catch (error) {
+            console.error('Deu ruim**', error.stack)
+            return this.handleError({ statusCode: 500 })
+        }
+    }
+}
+
+// factory
+const AWS = require('aws-sdk')
+const dynamoDB = new AWS.DynamoDB.DocumentClient()
+const handler = new Handler({
+    dynamoDBSvc: dynamoDB
+})
+module.exports = decoratorValidator(
+    handler.main.bind(handler),
+    Handler.validator(),
+    globalEnum.ARG_TYPE.BODY)
